70e0c413468ce40666d93453eea5340c4dff9b93
viperisopp <michaely6828@gmail.com>
2025-09-06 23:05:26 -0700

    Print, Input, and CS8600

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ namespace Learning
     {
         public static void Main(string[] args)
         {
+            // basic start
             Console.WriteLine("Hello World!");
 
             //creating/learing data types
@@ -14,6 +15,12 @@ namespace Learning
             // declaration of vars
             bool male;
             male = true;
+
+            // Print and Input
+            Console.WriteLine($"The number I choose was {num}, the name I choose was {name} and the float I choose was {funny_num}. Am I a male? {male}");
+            Console.WriteLine("What is your favorite song:");
+            string text = Console.ReadLine()!; // learned that ! to suppress CS8600 error
+            Console.WriteLine($"My favorite song is: {text}");
         }
     }
 }
