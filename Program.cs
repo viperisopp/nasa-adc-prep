f29a627f28216c9ec711b6ee96c2f6a419603b85
viperisopp <michaely6828@gmail.com>
2025-09-06 22:44:17 -0700

    c# setup, first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Learning
+{
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
