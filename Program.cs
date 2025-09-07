52e5db340d773d4d2f782c13ee87ba4d6f539459
viperisopp <michaely6828@gmail.com>
2025-09-06 23:24:06 -0700

    dtype conversion

--- Program.cs
+++ Program.cs
@@ -9,18 +9,33 @@ namespace Learning
             Console.WriteLine("Hello World!");
 
             //creating/learing data types
-            int num = 69;
-            string name = "michael";
-            float funny_num = 69.420f;
-            // declaration of vars
-            bool male;
-            male = true;
+            // int num = 69;
+            // string name = "michael";
+            // float funny_num = 69.420f;
+            // // declaration of vars
+            // bool male;
+            // male = true;
 
             // Print and Input
-            Console.WriteLine($"The number I choose was {num}, the name I choose was {name} and the float I choose was {funny_num}. Am I a male? {male}");
-            Console.WriteLine("What is your favorite song:");
-            string text = Console.ReadLine()!; // learned that ! to suppress CS8600 error
-            Console.WriteLine($"My favorite song is: {text}");
+            // Console.WriteLine($"The number I choose was {num}, the name I choose was {name} and the float I choose was {funny_num}. Am I a male? {male}");
+            // Console.WriteLine("What is your favorite song:");
+            // string text = Console.ReadLine()!; // learned that ! to suppress CS8600 error
+            // Console.WriteLine($"My favorite song is: {text}");
+
+            // Data type conversion
+            float num = 67.49f;
+            Console.WriteLine(num);
+            string text_num = Convert.ToString(num);
+            Console.WriteLine(text_num);
+            float num_again = Convert.ToSingle(text_num);
+            Console.WriteLine(num_again);
+            int funky_num = Convert.ToInt32(num_again); // rounds a flaot
+            Console.WriteLine(funky_num);
+
+            Console.WriteLine("Yo, enter a int please:");
+            int myInt = Convert.ToInt32(Console.ReadLine()); // can put zeros in front
+            Console.WriteLine(funky_num + myInt); // C# allows this, int + float, and operations within prints
+
         }
     }
 }
