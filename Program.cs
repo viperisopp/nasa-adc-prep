17bace54061021d9f9a8baaaced83d02d37ac59e
viperisopp <michaely6828@gmail.com>
2025-09-06 23:37:54 -0700

    randomness and doubles

--- Program.cs
+++ Program.cs
@@ -23,18 +23,23 @@ namespace Learning
             // Console.WriteLine($"My favorite song is: {text}");
 
             // Data type conversion
-            float num = 67.49f;
-            Console.WriteLine(num);
-            string text_num = Convert.ToString(num);
-            Console.WriteLine(text_num);
-            float num_again = Convert.ToSingle(text_num);
-            Console.WriteLine(num_again);
-            int funky_num = Convert.ToInt32(num_again); // rounds a flaot
-            Console.WriteLine(funky_num);
+            // float num = 67.49f;
+            // Console.WriteLine(num);
+            // string text_num = Convert.ToString(num);
+            // Console.WriteLine(text_num);
+            // float num_again = Convert.ToSingle(text_num);
+            // Console.WriteLine(num_again);
+            // int funky_num = Convert.ToInt32(num_again); // rounds a flaot
+            // Console.WriteLine(funky_num);
 
-            Console.WriteLine("Yo, enter a int please:");
-            int myInt = Convert.ToInt32(Console.ReadLine()); // can put zeros in front
-            Console.WriteLine(funky_num + myInt); // C# allows this, int + float, and operations within prints
+            // Console.WriteLine("Yo, enter a int please:");
+            // int myInt = Convert.ToInt32(Console.ReadLine()); // can put zeros in front
+            // Console.WriteLine(funky_num + myInt); // C# allows this, int + float, and operations within prints
+
+            Random rand = new Random(); // create object rand of class random, Next, NextBytes, NextDouble
+            int num1 = rand.Next(); // non neg random int
+            double num2 = rand.NextDouble(); // 0.0 <= x < 1.0, double data type better than float dt??
+            Console.WriteLine(num1 + num2);
 
         }
     }
