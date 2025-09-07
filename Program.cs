f7b322d99279f2be605265dbcbe9c48992590b83
viperisopp <michaely6828@gmail.com>
2025-09-06 22:48:33 -0700

    data types

--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@ namespace Learning
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //creating/learing data types
+            int num = 69;
+            string name = "michael";
+            float funny_num = 69.420f;
+            // declaration of vars
+            bool male;
+            male = true;
         }
     }
 }
